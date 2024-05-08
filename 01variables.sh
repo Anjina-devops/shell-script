86ad74283d78cbf52b2e31d3384957ed45a9ac8b
Anjina-devops <anjina0322@gmil.com>
2024-05-08 19:55:51 +0530

    hello

---
+++ 01variables.sh
@@ -0,0 +1,7 @@
+p1=$1 #this is the first value take from command line arguments
+p2=$2 #this is the second value take from command line arguments
+echo "$p1 $p2 are you this is my first shell script program"
+echo "$p1 "
+echo "$p1 $p2 "
+echo "$p1 $p2 are "
+echo "$p1 $p2 are you "
