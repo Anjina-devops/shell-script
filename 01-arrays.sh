aba96bd15d9e7030bd755a3681c2318cb8607371
Anjina-devops <anjina0322@gmil.com>
2024-05-08 21:23:41 +0530

    hello

---
+++ 01-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Arrays example to store multiple values in a single variable based on indexes starts from 0
+
+PERSONS=("Anjina" "Reddy" "Ammannigari")
+
+echo "First Person name :${PERSONS[0]}
+
+echo "All Persons names :${PERSONS[@]}
