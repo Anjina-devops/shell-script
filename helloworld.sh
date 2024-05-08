e0eb23c9b9459ce228e728d2e1db4db2f722ca44
Anjina-devops <anjina0322@gmil.com>
2024-05-08 19:23:57 +0530

    hello

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,7 @@
 # first shell script file
 #!/bin/bash
 echo "Hello how are you this is my first shell script program"
+echo "Hello "
+echo "Hello how "
+echo "Hello how are "
+echo "Hello how are you "
