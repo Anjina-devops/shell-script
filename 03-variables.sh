8001ddf8840071532168f9727aefd02a6f83bdd9
Anjina-devops <anjina0322@gmil.com>
2024-05-08 20:24:43 +0530

    hello

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #shell-script program to get input from the user through prompt dynamically
-
+# this is the values print only for the demo...real time we shuld not print user deatails like username and password
 echo "Please enter the username  :"
 
 read -s USERNAME
