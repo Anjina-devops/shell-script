69481f40c3a92e64a9a8e68b83ca911e7d57aa94
Anjina-devops <anjina0322@gmil.com>
2024-05-09 09:48:59 +0530

    hello

--- 06-install-mysql.sh
+++ 06-install-mysql.sh
@@ -6,5 +6,14 @@ then
     echo "This installation require root access....Please switch to as a Root User!!!!!!"
     exit 1
 fi
-
+# it is our responsible to check installation is success or not
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation is not success...Please check the logs"
+    exit 1
+else
+    echo "Installation is successfully completed....!!!"
+
+fi        
